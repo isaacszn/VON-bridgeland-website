e5e648716633da5078029e08a2c7e17f844e6649
Zik Szn <isaaczik9@gmail.com>
2025-06-28 10:48:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const showMenu = () => {
+  document.querySelector('aside').style.width = '70%'
+  document.querySelector('aside').style.height = '100%'
+}
+
+const hideMenu = () => {
+  document.querySelector('aside').style.width = '0%'
+}
